dc2d232c3398c4409633d5e93b7eb5a3a76851e1
Taylor Hodge <j.taylor.hodge@gmail.com>
2020-07-19 14:41:35 -0400

    refactor

--- server.rb
+++ server.rb
@@ -2,14 +2,22 @@ require 'socket'
 
 BUFFER_SIZE = 1024
 
+host = 'localhost'
+port = 4000
+
 socket = UDPSocket.new
-socket.bind('127.0.0.1', 1337)
+socket.bind(host, port)
 
 loop do
   message, sender = socket.recvfrom(BUFFER_SIZE)
-
   port = sender[1]
   host = sender[2]
+  remote = sender[3]
+
+  puts "#{remote} sent #{message}"
+
+  response = (message.to_i * 2).to_s
+  puts "We'll respond with #{message.upcase}"
 
   socket.send(message.upcase, 0, host, port)
 end
